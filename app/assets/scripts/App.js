77cf1ba45a7391c158e8e56bc0bef2548dbfe354
Navaraj Subedi <srizonsansar@gmail.com>
2021-08-09 21:47:01 +0100

    Completed the reveal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,12 @@
-import '../styles/styles.css';
+import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
-import MobileMenu from './modules/mobileMenu';
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
-
-/* Lesson Example Code */
